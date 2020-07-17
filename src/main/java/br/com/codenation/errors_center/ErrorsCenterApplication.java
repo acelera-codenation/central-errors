f963104d1dd2623885398fcc985b7ab8ab66ee3e
Samuel Santos <>
2020-07-16 21:27:30 -0300

    Start project

---
+++ src/main/java/br/com/codenation/errors_center/ErrorsCenterApplication.java
@@ -0,0 +1,13 @@
+package br.com.codenation.errors_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorsCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorsCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/codenation/errors_center/ErrorsCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codenation.errors_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorsCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
