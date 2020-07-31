a9e7be535cfda35e85ee22b5659b13c6f35ab17d
Samuel Santos <>
2020-07-31 17:46:16 -0300

    Fix build

--- src/test/java/br/com/central/errors/AppTests.java
+++ src/test/java/br/com/central/errors/AppTests.java
@@ -1,9 +1,14 @@
 package br.com.central.errors;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest(classes = App.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 class AppTests {
 
 	@Test
