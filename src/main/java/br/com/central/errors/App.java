cf973f35c642dd3f80fd5c0ad8495f67b9f6f4ea
samuelssantos <samuelsantos.sjdr@gmail.com>
2020-08-18 10:51:43 -0300

    Adicionar redirect home

--- src/main/java/br/com/central/errors/App.java
+++ src/main/java/br/com/central/errors/App.java
@@ -3,13 +3,21 @@ package br.com.central.errors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 @EnableJpaAuditing
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/swagger-ui.html";
     }
 
 }
