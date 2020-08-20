1f5605d8eacb67ae158e04fc02ef11fbc10a3965
samuelssantos <samuelsantos.sjdr@gmail.com>
2020-08-20 17:54:23 -0300

    Coverage

--- src/main/java/br/com/central/errors/App.java
+++ src/main/java/br/com/central/errors/App.java
@@ -2,13 +2,16 @@ package br.com.central.errors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @Controller
 @EnableJpaAuditing
+@EnableCaching
 public class App {
 
     public static void main(String[] args) {
